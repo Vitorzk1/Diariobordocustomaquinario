85c656df14ea85dfc536fb5a77267bd74cf0d569
Eric Kauan Tering Couto <2024105070074@aluno.ifro.edu.br>
2026-09-14 09:57:49 -0400

    Adiciona app Flask

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+registros = []
+
+@app.route("/")
+def inicio():
+    return redirect(url_for("registro_horas"))
+
+@app.route("/registro-horas", methods=["GET", "POST"])
+def registro_horas():
+    if request.method == "POST":
+        maquina = request.form["maquina"]
+        data = request.form["data"]
+        horas = request.form["horas"]
+        descricao = request.form["descricao"]
+
+        registros.append({
+            "maquina": maquina,
+            "data": data,
+            "horas": horas,
+            "descricao": descricao
+        })
+
+        return redirect(url_for("registro_horas"))
+
+    return render_template(
+        "registro_horas.html",
+        registros=registros
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
